50ada00bff578f7221e4c66ed6466ab5f9fd3eab
shinwoocheol <swc9704@gmail.com>
2023-01-03 11:23:02 +0900

    Boot07 에 자료실기능 수정

--- Boot07_Final/src/main/java/com/gura/boot07/file/service/FileServiceImpl.java
+++ Boot07_Final/src/main/java/com/gura/boot07/file/service/FileServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,10 @@ public class FileServiceImpl implements FileService {
 	@Autowired
 	private FileDao dao;
 
+	// custom.properties 파일에 작성한 내용 읽어오기
+	@Value("${file.location}")
+	private String fileLocation;
+	
 	@Override
 	public void getList(HttpServletRequest request) {
 		// 한 페이지에 몇개씩 표시할 것인지
@@ -117,8 +122,8 @@ public class FileServiceImpl implements FileService {
 		// 파일의 크기
 		long fileSize = myFile.getSize();
 
-		// webapp/resources/upload 폴더 까지의 실제 경로(서버의 파일시스템 상에서의 경로)
-		String realPath = request.getServletContext().getRealPath("/resources/upload");
+		// 파일을 저장할 폴더의 실제 경로 C:\data
+		String realPath = fileLocation;
 		// 저장할 파일의 상세 경로
 		String filePath = realPath + File.separator;
 		// 디렉토리를 만들 파일 객체 생성
@@ -148,12 +153,9 @@ public class FileServiceImpl implements FileService {
 	}
 
 	@Override
-	public void getFileData(int num, ModelAndView mView) {
+	public FileDto getFileData(int num) {
 		// 다운로드할 파일의 정보를 얻어와
-		FileDto dto = dao.getData(num);
-		// ModelAndView 객체에 담아준다.
-		mView.addObject("dto", dto);
-
+		return dao.getData(num);
 	}
 
 	@Override
@@ -170,7 +172,7 @@ public class FileServiceImpl implements FileService {
 		
 		// 파일 시스템에서 삭제
 		String saveFileName = dto.getSaveFileName();
-		String path = request.getServletContext().getRealPath("/resources/upload") + File.separator + saveFileName;
+		String path = fileLocation + saveFileName;
 		new File(path).delete();
 		
 		// DB 에서 파일 정보 삭제
